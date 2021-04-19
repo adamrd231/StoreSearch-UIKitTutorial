f58c6e9e8ea9898b989d316fd17b4e681aebe9d7
adamrd231 <adam.rdconcepts@gmail.com>
2021-04-19 11:13:43 -0700

    Make calls to iTunes API, match results with an Enum for better naming and then sorting the results for displaying on the device.

--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -7,7 +7,94 @@
 
 import Foundation
 
-class SearchResult {
-    var name = ""
-    var artistName = ""
+class ResultArray: Codable {
+    var resultCount = 0
+    var results = [SearchResult]()
 }
+
+class SearchResult: Codable, CustomStringConvertible {
+    // Format the data object recieved from the iTunes Store
+    var description: String {
+        return "\nResult - Kind: \(String(describing: kind)), Name: \(name), Artist Name: \(artist), Price: \(price), Store URL: \(storeURL)"
+    }
+    
+    var name: String {
+        return trackName ?? collectionName ?? ""
+    }
+    
+    var storeURL: String {
+      return trackViewUrl ?? collectionViewUrl ?? ""
+    }
+
+    var type: String {
+        let kind = self.kind ?? "audiobook"
+        switch kind {
+            case "album": return "Album"
+            case "audiobook": return "Audio Book"
+            case "book": return "Book"
+            case "ebook": return "E-Book"
+            case "feature-movie": return "Movie"
+            case "music-video": return "Music Video"
+            case "podcast": return "Podcast"
+            case "software": return "App"
+            case "song": return "Song"
+            case "tv-episode": return "TV Episode"
+            default: break
+        }
+        return "Unkown"
+    }
+    
+    var artist: String {
+        return artistName ?? ""
+    }
+
+    var trackName: String? = ""
+    
+    var kind: String? = ""
+    var trackPrice: Double? = 0.0
+    var currency = ""
+    var artistName: String? = ""
+    var trackViewUrl: String?
+    var collectionName: String?
+    var collectionViewUrl: String?
+    var collectionPrice: Double?
+    var itemPrice: Double?
+    var itemGenre: String?
+    var bookGenre: [String]?
+    
+    var imageSmall = ""
+    var imageLarge = ""
+    
+    var genre: String {
+      if let genre = itemGenre {
+        return genre
+      } else if let genres = bookGenre {
+        return genres.joined(separator: ", ")
+      }
+      return ""
+    }
+    
+    var price: Double {
+      return trackPrice ?? collectionPrice ?? itemPrice ?? 0.0
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case imageSmall = "artworkUrl60"
+        case imageLarge = "artworkUrl100"
+        case itemGenre = "primaryGenreName"
+        case bookGenre = "genres"
+        case itemPrice = "price"
+        case kind, artistName, currency
+        case trackName, trackPrice, trackViewUrl
+        case collectionName, collectionViewUrl, collectionPrice
+    }
+
+    
+    
+
+}
+
+func < (lhs: SearchResult, rhs: SearchResult) -> Bool {
+    return lhs.name.localizedStandardCompare(rhs.name) == .orderedAscending
+}
+
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -26,7 +26,7 @@ class SearchViewController: UIViewController {
         searchBar.becomeFirstResponder()
     }
 
-    //: MARK: Variables
+    // MARK: Variables
     // Variables connected to storyboard
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
@@ -43,26 +43,80 @@ class SearchViewController: UIViewController {
         }
     }
     
+    // MARK: Helper Functions
+    func iTunesURL(searchText: String) -> URL {
+        let encodedText = searchText.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+        let urlString = "https://itunes.apple.com/search?term=\(encodedText)"
+        let url = URL(string: urlString)
+        return url!
+    }
+    
+    func performStoreRequest(with url: URL) -> Data? {
+        do {
+            return try Data(contentsOf: url)
+        } catch {
+            print("Download Error: \(error.localizedDescription)")
+            showNetworkError()
+            return nil
+        }
+    }
+    
+    func parse(data: Data) -> [SearchResult] {
+        do {
+            let decoder = JSONDecoder()
+            let result = try decoder.decode(ResultArray.self, from: data)
+            return result.results
+        } catch {
+            print("JSON Error: \(error)")
+            return []
+        }
+    }
+    
+    func showNetworkError() {
+        let alert = UIAlertController(
+            title:"Whoops...",
+            message: "There was an error accessing iTunes server" + "\nPlease Try Again",
+            preferredStyle: .alert
+        )
+        
+        let action = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil
+        )
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
 
 // MARK: SearchBar View Controller
 extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-      searchResults = []
+        if !searchBar.text!.isEmpty {
+            // Mark if user has searched or not
+            userHasSearched = true
+            // Clear the search results array for new searching
+            searchResults = []
+            
+            // Contact Itunes and gather the URL for search
+            let url = iTunesURL(searchText: searchBar.text!)
+            print(url)
+            if let data = performStoreRequest(with: url) {
+                searchResults = parse(data: data)
+                searchResults.sort(by: <)
+            }
+            
+
+            tableView.reloadData()
+            
+        }
         
-    if searchBar.text! != "Justin" {
-    // Fake Search Result to populate queries with data
-      for i in 0...2 {
-        let searchResult = SearchResult()
-        searchResult.name = "Fake search result for \(i)"
-        searchResult.artistName = "Search bar result: \(searchBar.text!)"
-        searchResults.append(searchResult)
-      }
-    }
-        userHasSearched = true
-        tableView.reloadData()
-        searchBar.resignFirstResponder()
+        
+        
+        
+        
 }
     
     func position(for bar: UIBarPositioning) -> UIBarPosition {
@@ -98,10 +152,17 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier:
           TableView.CellIdentifiers.searchResultCell,
           for: indexPath) as! SearchResultCell
-
+        
+        // Pull up the searchResult array to modify
         let searchResult = searchResults[indexPath.row]
+        
         cell.nameLabel.text = searchResult.name
-        cell.artistNameLabel.text = searchResult.artistName
+        // Check if there is artist information in the cell
+        if searchResult.artist.isEmpty {
+            cell.artistNameLabel.text = ""
+        } else {
+            cell.artistNameLabel.text = "\(searchResult.artist), (\(searchResult.type))"
+        }
         return cell
       }
     }
